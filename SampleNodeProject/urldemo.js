79aa87804725892c7921dedc8c100feb603d780e
vinaneosoft <vinaneosoft@gmail.com>
2024-08-06 11:27:01 +0530

    url demo

--- SampleNodeProject/urldemo.js
+++ SampleNodeProject/urldemo.js
@@ -15,6 +15,22 @@ const myURL =
   console.log(urlObject.href);
   console.log(urlObject.hostname);
   console.log(urlObject.pathname);
-  console.log(urlObject.origin);
+  console.log("origin", urlObject.origin);
   console.log(urlObject.password);
+  console.log(urlObject.protocol);
 
+
+
+const origin="https://en.wikipedia.org"
+const pathname = '/wiki/Sachin_Tendulkar';
+const hash = '#References';
+const newURL=new URL(`${origin}${pathname}${hash}`)
+console.log(newURL);
+console.log(newURL.href);
+
+
+const partialURL = new URL('https://example.org');
+partialURL.pathname = '/a/b/c';
+partialURL.search = '?d=e';
+partialURL.hash = '#hashvalue'; 
+console.log(partialURL.href);
