2f15f635b5d2ef179db0e2057bcc9e03510c4729
vinaneosoft <vinaneosoft@gmail.com>
2024-08-06 17:32:35 +0530

    crud mongoose

--- SampleNodeProject/mongodb.js
+++ SampleNodeProject/mongodb.js
@@ -6,6 +6,13 @@ const mongoose=require("mongoose");
     getAllEmployees();
     getEmployeeById(123);
     deleteEmployeeById(111);
+    const employee={
+        empId:123,
+        empName:"kiran patil",
+        empEmail:"kiran@gmail.com",
+        empSalary : 74000
+    }
+    updateEmployee(employee);
     }
 )
 .catch(err=>console.log(err))
@@ -17,19 +24,36 @@ const empSchema=new mongoose.Schema({
 });
 // note : in mongodb database, collection name must be in plural form
 const EmployeeModel=mongoose.model("Employee",empSchema) ;
+
 async function getAllEmployees(){
     // find all records from collection
      const data= await EmployeeModel.find({}).exec();
      console.log(data);
+     console.log("_____________");
+     
 }
 async function getEmployeeById(empId){
     // find all records from collection
-     const data= await EmployeeModel.findById(empId).exec();
+     const data= await EmployeeModel.findById(empId).exec(); // _id
      console.log(data);
+     console.log("_____________");
 }
 
 async function deleteEmployeeById(empId){
     // find all records from collection
      const data= await EmployeeModel.deleteOne({_id:empId});
      console.log(data);
+     console.log("_____________");
+}
+
+async function updateEmployee(employee){
+    // find all records from collection
+     const data= await EmployeeModel.updateOne({_id:employee.empId}, 
+        {
+            emp_name:employee.empName, 
+            emp_email:employee.empEmail,
+            emp_salary : employee.empSalary
+        } );
+     console.log(data);
+     console.log("_____________");
 }
