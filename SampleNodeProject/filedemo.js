a2b87a6e4992572840fb45e3a2e98065dc0a9f36
vinaneosoft <vinaneosoft@gmail.com>
2024-08-06 15:18:57 +0530

    read

--- SampleNodeProject/filedemo.js
+++ SampleNodeProject/filedemo.js
@@ -11,9 +11,12 @@ console.log("___________________");
 async function  readData(){
     const fd= await open("./Files/Mangeshkar.txt")
     let array=new Buffer.alloc(50); // 50 bytes
-    fd.read(array,0, 15, 3).then((object)=>{
-        console.log(object.bytesRead);
-        console.log(object.buffer.toString());
-    });
+    fd.read(array,0, 15, 3)
+        .then(object=>{
+            console.log(object.bytesRead);
+            console.log(object.buffer.toString());
+        })
+        .catch(errr=>console.log(errr))
+        .finally(()=>{})
 }
 readData();
