0f212960bf383e3fdbb6aae908b1e445c1afa137
vinaneosoft <vinaneosoft@gmail.com>
2024-08-08 13:03:35 +0530

    commit

--- SampleNodeProject/NodeStudy/jsondemo.js
+++ SampleNodeProject/NodeStudy/jsondemo.js
@@ -6,9 +6,21 @@ try{
 fs.readFile(file, (error, data)=>{
     if(error)
             throw new Error("error in reading a file");
-     console.log(data.toString());  
+    // console.log(data.toString());  
+    const jsondata=JSON.parse(data);
+ //   console.log(jsondata);
+    const array= jsondata.employeerecords;
+    console.log(array);
+    test(array);
 })}
 catch(err){
     console.log(err);
 }
 console.log("further operation.......");
+
+
+function test(array){
+    const emps=array.filter(emp=>emp.salary==45000)
+    console.log(emps);
+    
+}
